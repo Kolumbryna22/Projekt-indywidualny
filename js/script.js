d943d9ecf68bd38984b4850c5eada51d6352d1bd
Dominik Wilusz <wilusz1@o2.pl>
2018-07-22 19:41:45 +0200

    Add responsive header

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@
 
     var button = document.getElementById('hamburger');
     var nav = document.getElementById('nav');
+    var header = document.getElementById('header');
     var section = document.getElementsByTagName('section');
-    var change = true;
+    var change = false;
 
     var changeSection = function(x) {
         var i = 0;
@@ -17,10 +18,12 @@
     window.addEventListener('resize', function(){
         if(window.innerWidth > 688) {
             nav.style.display = "block";
+            header.style.display = "flex";
             change = false;
         }
         else {
             nav.style.display = "none";
+            header.style.display = "none";
             change = true;
         }
         changeSection(!change);
@@ -28,6 +31,9 @@
 
     var changeVisible = function() {
         change ? (nav.style.display = "block") : (nav.style.display = "none");
+        if(window.innerWidth < 688) {
+            change ? (header.style.display = "flex") : (header.style.display = "none");
+        }
         if(window.innerWidth > 688) {
             changeSection(change);
         }
