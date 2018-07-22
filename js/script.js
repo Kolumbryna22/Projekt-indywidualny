ba030300a92fae6d1049468983aa5feb05621eae
Dominik Wilusz <wilusz1@o2.pl>
2018-07-22 18:31:34 +0200

    Fix some issues

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@
 
     var button = document.getElementById('hamburger');
     var nav = document.getElementById('nav');
+    var section = document.getElementsByTagName('section');
     var change = true;
+
+    var changeSection = function(x) {
+        var i = 0;
+        while(section[i]) {
+            section[i].style.paddingLeft = x ? "203px" : "15px";
+            i++;
+        }
+    };
 
     window.addEventListener('resize', function(){
         if(window.innerWidth > 688) {
@@ -12,13 +21,18 @@
         }
         else {
             nav.style.display = "none";
+            change = true;
         }
-    })
+        changeSection(!change);
+    });
 
     var changeVisible = function() {
         change ? (nav.style.display = "block") : (nav.style.display = "none");
+        if(window.innerWidth > 688) {
+            changeSection(change);
+        }
         change = !change;
-    }
+    };
 
     button.addEventListener('click', changeVisible);
 })();
