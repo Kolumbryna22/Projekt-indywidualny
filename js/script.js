9cd14e44062a54f4fff2eb7bba680e884fbd074c
Dominik Wilusz <wilusz1@o2.pl>
2018-07-29 14:39:40 +0200

    Add chart in index.html

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
     var button = document.getElementById('hamburger');
     var nav = document.getElementById('nav');
     var navigationHeader = document.getElementById('navigationHeader');
-    var modalLinks = document.getElementById('links');
-    var modalBanners = document.getElementById('banners');
     var linksButton = document.getElementById('linksButton');
     var bannersButton = document.getElementById('bannersButton');
     var linksModal = document.getElementById('linksModal');
@@ -13,6 +11,7 @@
     var closeLinks = document.getElementById('closeLinks');
     var closeBanners = document.getElementById('closeBanners');
     var change = window.innerWidth > 688 ? false : true;
+    var ctx = document.getElementById('myChart').getContext('2d');
 
     //functions for responsive web
     var changeSection = function(x) {
@@ -64,6 +63,40 @@
         bannersModal.style.display = "none";
     };
 
+    //chart
+    var chart = new Chart(ctx, {
+        // 1
+        type: 'bar',
+        data: {
+            // 2
+            labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+            // 3
+            datasets: [{
+                // 4
+                label: "Signups",
+                // 5
+                backgroundColor: '#8DBEC8',
+                borderColor: '#8DBEC8',
+                // 6
+                data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+            },
+            {
+                label: "FTD",
+                backgroundColor: '#F29E4E',
+                borderColor: '#F29E4E',
+                data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+            },
+            {
+                label: "Earned",
+                backgroundColor: '#71B374',
+                borderColor: '#71B374',
+                data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+                // 7
+                hidden: true,
+            }]
+        },
+    });
+    
     //listeners for action
     button.addEventListener('click', changeVisible);
     linksButton.addEventListener('click', addVisibleLinks);
