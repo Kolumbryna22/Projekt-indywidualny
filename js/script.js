23224ae4240a93d0b5fe6af40028b39e2f224309
Dominik Wilusz <wilusz1@o2.pl>
2018-07-29 15:26:28 +0200

    Fix some issues

--- js/script.js
+++ js/script.js
@@ -68,16 +68,11 @@
         // 1
         type: 'bar',
         data: {
-            // 2
             labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-            // 3
             datasets: [{
-                // 4
                 label: "Signups",
-                // 5
                 backgroundColor: '#8DBEC8',
                 borderColor: '#8DBEC8',
-                // 6
                 data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
             },
             {
@@ -91,7 +86,6 @@
                 backgroundColor: '#71B374',
                 borderColor: '#71B374',
                 data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-                // 7
                 hidden: true,
             }]
         },
