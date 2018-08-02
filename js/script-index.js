016a8ddff0137681f8f97602d9621f62c073561b
Dominik Wilusz <wilusz1@o2.pl>
2018-08-02 18:24:46 +0200

    Add script-index.js

--- js/script-index.js
+++ js/script-index.js
@@ -11,6 +11,7 @@
     var closeLinks = document.getElementById('closeLinks');
     var closeBanners = document.getElementById('closeBanners');
     var change = window.innerWidth > 688 ? false : true;
+    var ctx = document.getElementById('myChart').getContext('2d');
 
     //functions for responsive web
     var changeSection = function(x) {
@@ -61,6 +62,34 @@
     var disableVisibleBanners = function() {
         bannersModal.style.display = "none";
     };
+
+    chart
+    var chart = new Chart(ctx, {
+        // 1
+        type: 'bar',
+        data: {
+            labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+            datasets: [{
+                label: "Signups",
+                backgroundColor: '#8DBEC8',
+                borderColor: '#8DBEC8',
+                data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+            },
+            {
+                label: "FTD",
+                backgroundColor: '#F29E4E',
+                borderColor: '#F29E4E',
+                data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+            },
+            {
+                label: "Earned",
+                backgroundColor: '#71B374',
+                borderColor: '#71B374',
+                data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+                hidden: true,
+            }]
+        },
+    });
     
     //listeners for action
     button.addEventListener('click', changeVisible);
